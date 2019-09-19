626e3b57a78fc20aa18e6eb771a80248b0d55173
berdmival <mtsby7049874@gmail.com>
2019-09-19 14:08:19 +0300

    fix imports in calc

--- src/main/java/by/tms/spring/calc/Calc.java
+++ src/main/java/by/tms/spring/calc/Calc.java
@@ -4,7 +4,6 @@ import by.tms.spring.action.ActionType;
 import by.tms.spring.util.Validator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service("calc")
